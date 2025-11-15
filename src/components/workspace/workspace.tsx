a1b016edf95644c6168ee528238d39d8d083a91e
ShroukMagdy3 <shorokmagdy15@gmail.com>
2025-11-15 14:58:50 +0200

    toast

--- src/components/workspace/workspace.tsx
+++ src/components/workspace/workspace.tsx
@@ -6,15 +6,15 @@ import UploadMenu from "../upload/upload";
 import { ClipLoader } from "react-spinners";
 import toast from "react-hot-toast";
 interface workspace {
-    _id:string,
-    name:string
-    createdAt:string
+  _id: string;
+  name: string;
+  createdAt: string;
 }
 
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 export default function WorkspaceProfile() {
-  const [workspace, setWorkspace] = useState<workspace >();
+  const [workspace, setWorkspace] = useState<workspace>();
   const [documents, setDocuments] = useState<[]>([]);
   const [loading, setLoading] = useState(true);
   const [docsLoading, setDocsLoading] = useState(true);
@@ -24,12 +24,16 @@ export default function WorkspaceProfile() {
       try {
         const { data } = await axios.get(
           `${VITE_API_URL}/api/v1/workspaces/getMyWorkspace`,
-          { headers: { authorization: `bearer ${localStorage.getItem("accessToken")}` } }
+          {
+            headers: {
+              authorization: `bearer ${localStorage.getItem("accessToken")}`,
+            },
+          }
         );
         setWorkspace(data.workspace);
       } catch (err) {
         console.error(err);
-        toast.error("error")
+        toast.error("error");
       } finally {
         setLoading(false);
       }
@@ -43,7 +47,7 @@ export default function WorkspaceProfile() {
       setDocsLoading(true);
       try {
         const { data } = await getAllDocuments();
-          setDocuments(data.docs);
+        setDocuments(data.docs);
       } catch (err) {
         console.error(err);
       } finally {
@@ -54,26 +58,43 @@ export default function WorkspaceProfile() {
   }, [workspace]);
 
   const handleUpdate = async (newName: string) => {
+    try {
       const { data } = await axios.patch(
-        `${VITE_API_URL}/api/v1/workspaces/update/${workspace?._id}`,
-        { name: newName },
-        { headers: { authorization: `bearer ${localStorage.getItem("accessToken")}` } }
-      );
-      setWorkspace((prev) => {
-  if (!prev) return prev;
-  return { ...prev, name: newName };
-});
+      `${VITE_API_URL}/api/v1/workspaces/update/${workspace?._id}`,
+      { name: newName },
+      {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+      }
+    );
+    setWorkspace((prev) => {
+      if (!prev) return prev;
+      return { ...prev, name: newName };
+    });
+    toast.success("Name Updated")
+    } catch (error) {
+      toast.error("Please try in another time!")
+    }
   };
 
   if (loading)
-    return <div className="flex justify-center items-center h-full min-h-[200px]">
+    return (
+      <div className="flex justify-center items-center h-full min-h-[200px]">
         <ClipLoader size={50} color="#fbbf24" />
       </div>
+    );
   return (
     <div className="min-h-screen  rounded-lg text-white flex flex-col items-center">
-      {workspace && <WorkspaceHeader workspaceName={workspace.name} createdAt={workspace.createdAt} onUpdate={handleUpdate} />}
-      
-      { workspace && <UploadMenu workspaceId={workspace?._id}  />}
+      {workspace && (
+        <WorkspaceHeader
+          workspaceName={workspace.name}
+          createdAt={workspace.createdAt}
+          onUpdate={handleUpdate}
+        />
+      )}
+
+      {workspace && <UploadMenu workspaceId={workspace?._id} />}
     </div>
   );
 }
